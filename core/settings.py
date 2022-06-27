2ea49f07d62e04d9b421fa588d3bc9341e14a16a
Mehemmed Mustafayev <mohammedmustafayev@gmail.com>
2022-06-27 17:30:28 +0400

    version-1.1.0 database added

--- core/settings.py
+++ core/settings.py
@@ -10,17 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 
+
+from pathlib import Path
+import os
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env()
+environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_jb&w%ya41gjfj90^z)5fic$7xu)8kh*tk5_y6=ywdj@7*kqar'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("POSTGRES_DB"),
+        'USER': env("POSTGRES_USER"), 
+        'PASSWORD': env("POSTGRES_PASSWORD"),
+        'HOST': "db",
+        'PORT': "5432",
     }
 }
 
